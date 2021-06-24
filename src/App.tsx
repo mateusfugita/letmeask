1024ae9f04117b26fc7528a13ff1e518f3cfe19f
Mateus Fugita <mateushiwao@gmail.com>
2021-06-24 00:22:38 -0300

    create home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { Button } from './components/Button';
+import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <Button />
+    <Home />
   );
 }
 
